adb3049e2d00f83ca73f3bbe84358be20c98fc1e
lingcraft <lingcraft@qq.com>
2022-08-22 09:42:25 +0800

    优化

--- ecloud.py
+++ ecloud.py
@@ -14,14 +14,14 @@ corp_id, corp_secret, to_user, agent_id, media_id = os.getenv("PUSHER_WECHAT").s
 session = requests.Session()
 sio = StringIO()
 sio.seek(0, 2)  # 将读写位置移动到结尾
-tz = pytz.timezone('Asia/Shanghai')
+tz = pytz.timezone("Asia/Shanghai")
 time_now = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
 sio.write("-----------" + time_now + "----------\n")
 
 
 def main():
     if username == "" or password == "":
-        sio.write('签到失败：用户名或密码为空，请设置\n')
+        sio.write("签到失败：用户名或密码为空，请设置\n")
         desc = sio.getvalue()
         push(desc)
         return None
@@ -33,45 +33,45 @@ def main():
     else:
         pass
     rand = str(round(time.time() * 1000))
-    surl = f'https://api.cloud.189.cn/mkt/userSign.action?rand={rand}&clientType=TELEANDROID&version=8.6.3&model=SM-G930K'
-    url = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN&activityId=ACT_SIGNIN'
-    url2 = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN_PHOTOS&activityId=ACT_SIGNIN'
+    surl = f"https://api.cloud.189.cn/mkt/userSign.action?rand={rand}&clientType=TELEANDROID&version=8.6.3&model=SM-G930K"
+    url = f"https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN&activityId=ACT_SIGNIN"
+    url2 = f"https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN_PHOTOS&activityId=ACT_SIGNIN"
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G930K Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Ecloud/8.6.3 Android/22 clientId/355325117317828 clientModel/SM-G930K imsi/460071114317824 clientChannelId/qq proVersion/1.0.6',
+        "User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; SM-G930K Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Ecloud/8.6.3 Android/22 clientId/355325117317828 clientModel/SM-G930K imsi/460071114317824 clientChannelId/qq proVersion/1.0.6",
         "Referer": "https://m.cloud.189.cn/zhuanti/2016/sign/index.jsp?albumBackupOpened=1",
         "Host": "m.cloud.189.cn",
         "Accept-Encoding": "gzip, deflate",
     }
     # 签到
     response = session.get(surl, headers=headers)
-    bonus = response.json()['netdiskBonus']
-    if response.json()['isSign'] == "false":
+    bonus = response.json()["netdiskBonus"]
+    if response.json()["isSign"] == "false":
         sio.write(f"签到提示：未签到，签到获得{bonus}M空间\n")
     else:
         sio.write(f"签到提示：已经签到过了，签到获得{bonus}M空间\n")
     # 第一次抽奖
     response = session.get(url, headers=headers)
     if "errorCode" in response.text:
-        if response.json()['errorCode'] == "User_Not_Chance":
+        if response.json()["errorCode"] == "User_Not_Chance":
             sio.write("第一次抽奖：抽奖次数不足\n")
         else:
             sio.write("第一次抽奖失败\n")
             sio.write(response.text)
             sio.write("\n")
     else:
-        prize = response.json()['prizeName']
+        prize = response.json()["prizeName"]
         sio.write(f"第一次抽奖：抽奖获得{prize}\n")
     # 第二次抽奖
     response = session.get(url2, headers=headers)
     if "errorCode" in response.text:
-        if response.json()['errorCode'] == "User_Not_Chance":
+        if response.json()["errorCode"] == "User_Not_Chance":
             sio.write("第二次抽奖：抽奖次数不足\n")
         else:
             sio.write("第二次抽奖失败\n")
             sio.write(response.text)
             sio.write("\n")
     else:
-        prize = response.json()['prizeName']
+        prize = response.json()["prizeName"]
         sio.write(f"第二次抽奖：抽奖获得{prize}\n")
     desc = sio.getvalue()
     push(desc)
@@ -120,34 +120,34 @@ def b64tohex(a):
 def rsa_encode(j_rsakey, string):
     rsa_key = f"-----BEGIN PUBLIC KEY-----\n{j_rsakey}\n-----END PUBLIC KEY-----"
     pubkey = rsa.PublicKey.load_pkcs1_openssl_pem(rsa_key.encode())
-    result = b64tohex((base64.b64encode(rsa.encrypt(f'{string}'.encode(), pubkey))).decode())
+    result = b64tohex((base64.b64encode(rsa.encrypt(f"{string}".encode(), pubkey))).decode())
     return result
 
 
 def calculate_md5_sign(params):
-    return hashlib.md5('&'.join(sorted(params.split('&'))).encode('utf-8')).hexdigest()
+    return hashlib.md5("&".join(sorted(params.split("&"))).encode("utf-8")).hexdigest()
 
 
 def login(username, password):
     url = "https://cloud.189.cn/api/portal/loginUrl.action?redirectURL=https://cloud.189.cn/web/redirect.html"
     r = session.get(url)
     captchaToken = re.findall(r"captchaToken' value='(.+?)'", r.text)[0]
-    lt = re.findall(r'lt = "(.+?)"', r.text)[0]
+    lt = re.findall(r"lt = \"(.+?)\"", r.text)[0]
     returnUrl = re.findall(r"returnUrl = '(.+?)'", r.text)[0]
-    paramId = re.findall(r'paramId = "(.+?)"', r.text)[0]
-    j_rsakey = re.findall(r'j_rsaKey" value="(\S+)"', r.text, re.M)[0]
+    paramId = re.findall(r"paramId = \"(.+?)\"", r.text)[0]
+    j_rsakey = re.findall(r"j_rsaKey\" value=\"(\S+)\"", r.text, re.M)[0]
     session.headers.update({"lt": lt})
 
     username = rsa_encode(j_rsakey, username)
     password = rsa_encode(j_rsakey, password)
     url = "https://open.e.189.cn/api/logbox/oauth2/loginSubmit.do"
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/76.0',
-        'Referer': 'https://open.e.189.cn/',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/76.0",
+        "Referer": "https://open.e.189.cn/",
     }
     data = {
         "appKey": "cloud",
-        "accountType": '01',
+        "accountType": "01",
         "userName": f"{{RSA}}{username}",
         "password": f"{{RSA}}{password}",
         "validateCode": "",
@@ -157,27 +157,27 @@ def login(username, password):
         "paramId": paramId
     }
     r = session.post(url, data=data, headers=headers, timeout=5)
-    if r.json()['result'] == 0:
+    if r.json()["result"] == 0:
         sio.write("登录提示：")
-        sio.write(r.json()['msg'])
+        sio.write(r.json()["msg"])
         sio.write("\n")
     else:
-        msg = r.json()['msg']
+        msg = r.json()["msg"]
         sio.write("签到失败：登录出错\n")
         sio.write("错误提示：\n")
         sio.write(msg)
         sio.write("\n")
         return "error"
-    redirect_url = r.json()['toUrl']
+    redirect_url = r.json()["toUrl"]
     r = session.get(redirect_url)
     return session
 
 
 def push(content: str = None) -> str:
-    if '失败' in content:
-        title = '天翼云盘签到失败'
+    if "失败" in content:
+        title = "天翼云盘签到失败"
     else:
-        title = '天翼云盘签到成功'
+        title = "天翼云盘签到成功"
     wx = WeChat(corp_id, corp_secret, agent_id)
     if media_id is not None:
         response = wx.send_mpnews(title, content, media_id, to_user)
@@ -214,7 +214,7 @@ class WeChat:
             },
             "safe": 0
         }
-        send_msges = (bytes(json.dumps(data), 'utf-8'))
+        send_msges = (bytes(json.dumps(data), "utf-8"))
         response = requests.post(url, send_msges)
         return response
 
@@ -232,14 +232,14 @@ class WeChat:
                         "title": title,
                         "thumb_media_id": media_id,
                         "content_source_url": "",
-                        "content": message.replace('\n', '<br/>'),
+                        "content": message.replace("\n", "<br/>"),
                         "digest": message,
                     }
                 ]
             },
             "safe": 0
         }
-        send_msges = (bytes(json.dumps(data), 'utf-8'))
+        send_msges = (bytes(json.dumps(data), "utf-8"))
         response = requests.post(url, send_msges)
         return response
 
