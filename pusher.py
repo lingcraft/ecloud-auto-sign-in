b3ef7d816b912df8c624aa5546e3800b90acce2f
lingcraft <lingcraft@qq.com>
2026-06-22 08:42:00 +0800

    优化代码

--- pusher.py
+++ pusher.py
@@ -14,66 +14,70 @@ class WeChat:
         self.way = way
         self.corp_id, self.corp_secret, self.to_user, self.agent_id, self.media_id = params
 
-    def push(self, content: str):
-        if "失败" in content:
-            title = f"{self.way}签到失败"
-        else:
-            title = f"{self.way}签到成功"
+    def push(self, message):
+        tip = "失败" if "失败" in message else "成功"
+        title = f"{self.way}签到{tip}"
         if self.media_id is not None:
-            response = self.send_news(title, content)
+            return self.send_news(title, message)
         else:
-            message = title + "\n" + content
-            response = self.send_text(message)
-        return response
+            return self.send_text(title, message)
 
     def get_token(self):
-        url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={}&corpsecret={}".format(self.corp_id,
-                                                                                            self.corp_secret)
-        response = requests.get(url)
+        response = requests.get(
+            "https://qyapi.weixin.qq.com/cgi-bin/gettoken",
+            params={
+                "corpid": self.corp_id,
+                "corpsecret": self.corp_secret
+            }
+        )
         if response.status_code == 200:
-            return response.json()["access_token"]
+            return response.json().get("access_token")
         else:
             return ""
 
-    def send_text(self, message):
-        url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={}".format(self.get_token())
-        data = {
-            "touser": self.to_user,
-            "msgtype": "text",
-            "agentid": self.agent_id,
-            "text": {
-                "content": message
+    def send_text(self, title, message):
+        return requests.post(
+            "https://qyapi.weixin.qq.com/cgi-bin/message/send",
+            params={
+                "access_token": self.get_token(),
             },
-            "safe": 0
-        }
-        msg = (bytes(json.dumps(data), "utf-8"))
-        response = requests.post(url, msg)
-        return response
+            json={
+                "touser": self.to_user,
+                "msgtype": "text",
+                "agentid": self.agent_id,
+                "text": {
+                    "content": f"{title}\n{message}"
+                },
+                "safe": 0
+            }
+        )
 
     def send_news(self, title, message):
-        url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={}".format(self.get_token())
         if not message:
             message = title
-        data = {
-            "touser": self.to_user,
-            "msgtype": "mpnews",
-            "agentid": self.agent_id,
-            "mpnews": {
-                "articles": [
-                    {
-                        "title": title,
-                        "thumb_media_id": self.media_id,
-                        "content_source_url": "",
-                        "content": message.replace("\n", "<br/>"),
-                        "digest": message,
-                    }
-                ]
+        return requests.post(
+            "https://qyapi.weixin.qq.com/cgi-bin/message/send",
+            params={
+                "access_token": self.get_token(),
             },
-            "safe": 0
-        }
-        msg = (bytes(json.dumps(data), "utf-8"))
-        response = requests.post(url, msg)
-        return response
+            json={
+                "touser": self.to_user,
+                "msgtype": "mpnews",
+                "agentid": self.agent_id,
+                "mpnews": {
+                    "articles": [
+                        {
+                            "title": title,
+                            "thumb_media_id": self.media_id,
+                            "content_source_url": "",
+                            "content": message.replace("\n", "<br/>"),
+                            "digest": message,
+                        }
+                    ]
+                },
+                "safe": 0
+            }
+        )
 
 
 def set_retry(session):
